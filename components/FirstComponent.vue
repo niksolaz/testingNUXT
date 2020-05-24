409eb03fcb94ed53e7a822d2a36cdf8ab940e611
Nicola Solazzo <solazzo.nicola@gmail.com>
2020-05-24 19:01:35 +0200

    first commit

---
+++ components/FirstComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Name {{name}}</p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable eol-last */
+// eslint-disable-next-line
+/* eslint-disable */
+  export default {
+    data() {
+      return {
+        name: 'Nicola'
+      }
+    }
+  }
+</script>
