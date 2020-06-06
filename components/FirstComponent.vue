8d3a2c8453f8538f8234fad3f946282df91d0cc5
Nicola Solazzo <solazzo.nicola@gmail.com>
2020-06-06 15:32:39 +0200

    form component
    create a form for testing component

--- components/FirstComponent.vue
+++ components/FirstComponent.vue
@@ -1,6 +1,61 @@
 <template>
   <div>
-    <p>Name {{ name }}</p>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-5" label="Price:" label-for="input-5">
+        <b-form-select
+          id="input-5"
+          v-model="form.price"
+          :options="prices"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4">
+        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
@@ -11,7 +66,48 @@
   export default {
     data() {
       return {
-        name: 'Nicola'
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          price: null,
+          checked: []
+        },
+        foods: [
+          { text: 'Select One', value: null },
+          { text: 'Carrots', value: 'carrots' },
+          { text: 'Beans', value: 'beans' },
+          { text: 'Tomatoes', value: 'tomatoes' },
+          { text: 'Corn', value: 'corn' } 
+          ],
+        prices: [
+          { text: 'Select Price', value: null },
+          { text: '$ 10', value:'10'},
+          { text: '$ 20', value:'20'},
+          { text: '$ 30', value:'30'},
+          { text: '$ 40', value:'40'}
+          ],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.price = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
       }
     }
   }
--- test/FirstComponent.spec.js
+++ test/FirstComponent.spec.js
@@ -4,7 +4,29 @@ import First from '@/components/FirstComponent.vue'
 describe('First', () => {
     test('is a Vue instance', () => {
         const wrapper = mount(First);
+        const data = First.data();
+        const resultForm = {
+            email: 'solazzo.nicola@gmail.com',
+            name: 'nicola',
+            food: data.foods[2].value,
+            price: data.prices[2].value,
+            checked: [
+                "that",
+                "me"
+            ]
+        }
         expect(wrapper.isVueInstance()).toEqual(true)
-        expect(First.data()).toEqual({ name: 'Nicola' })
+        expect(data.foods[2].value).toEqual('beans')
+        expect(resultForm).toEqual({
+            email: 'solazzo.nicola@gmail.com',
+            name: 'nicola',
+            food: 'beans',
+            price: '20',
+            checked: [
+                "that",
+                "me"
+            ]
+        })
+        console.log(data)
     })
 })
